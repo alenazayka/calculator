79e547633c3edd0412c7321c0d12ae05d95d9edc
alenazayka <56794815+alenazayka@users.noreply.github.com>
2019-10-25 18:43:09 +0300

    Add files via upload

--- Calc.java
+++ Calc.java
@@ -6,6 +6,7 @@ public class Calc {
         String expr = EnteringTheString.gettingString();
         char num1, num2;
         int res = 0;
+        String fres ;
         Pattern p = Pattern.compile("[A-Z]+");
         Matcher m = p.matcher(expr);
 
@@ -43,7 +44,8 @@ public class Calc {
                     n1 = RomanianNums.beginning(expr, n1);
                     n2 = RomanianNums.ending(expr, n2);
                     res = CalcMethods.calc(n1, n2, res, expr);
-                    System.out.println("Результат: " + res);
+                    fres = RomanianNums.transl(res);
+                    System.out.println("Результат: " + fres);
                 } else {
                     throw new SomethingWentWrong("Введите выражение корректно");
                 }
@@ -76,32 +78,95 @@ static class EnteringTheString {
         }
 }
 public static class RomanianNums {
-        public static int beginning(String str, int num){  //метод для работы с римскими цифрами, который обрабатывает
-            if (str.startsWith("VIII")) {                 //начало строки и присваивает значение первой переменной
+        public static int beginning(String str, int num) throws SomethingWentWrong {  //метод для работы с римскими цифрами, который обрабатывает
+            if (str.startsWith("VIII+", 0)) {
                 num = 8;
-            } else if (str.startsWith("III")) {
+            } else if (str.startsWith("VIII-", 0)) {
+                num = 8;
+            } else if (str.startsWith("VIII*", 0)) {
+                num = 8;
+            } else if (str.startsWith("VIII/", 0)) {
+                num = 8;
+            } else if (str.startsWith("III-", 0)) {
                 num = 3;
-            } else if (str.startsWith("VII")) {
+            } else if (str.startsWith("III+", 0)) {
+                num = 3;
+            } else if (str.startsWith("III*", 0)) {
+                num = 3;
+            } else if (str.startsWith("III/", 0)) {
+                num = 3;
+            } else if (str.startsWith("VII-", 0)) {
                 num = 7;
-            } else if (str.startsWith("IV")) {
+            } else if (str.startsWith("VII+", 0)) {
+                num = 7;
+            } else if (str.startsWith("VII*", 0)) {
+                num = 7;
+            } else if (str.startsWith("VII/", 0)) {
+                num = 7;
+            } else if (str.startsWith("IV-", 0)) {
                 num = 4;
-            } else if (str.startsWith("IX")) {
+            } else if (str.startsWith("IV+", 0)) {
+                num = 4;
+            } else if (str.startsWith("IV*", 0)) {
+                num = 4;
+            } else if (str.startsWith("IV/", 0)) {
+                num = 4;
+            } else if (str.startsWith("IX-", 0)) {
                 num = 9;
-            } else if (str.startsWith("VI")) {
+            } else if (str.startsWith("IX+", 0)) {
+                num = 9;
+            } else if (str.startsWith("IX*", 0)) {
+                num = 9;
+            } else if (str.startsWith("IX/", 0)) {
+                num = 9;
+            } else if (str.startsWith("VI-", 0)) {
                 num = 6;
-            } else if (str.startsWith("II")) {
+            } else if (str.startsWith("VI+", 0)) {
+                num = 6;
+            } else if (str.startsWith("VI*", 0)) {
+                num = 6;
+            } else if (str.startsWith("VI/", 0)) {
+                num = 6;
+            } else if (str.startsWith("II-", 0)) {
                 num = 2;
-            } else if (str.startsWith("I")) {
+            } else if (str.startsWith("II+", 0)) {
+                num = 2;
+            } else if (str.startsWith("II*", 0)) {
+                num = 2;
+            } else if (str.startsWith("II/", 0)) {
+                num = 2;
+            } else if (str.startsWith("I-", 0)) {
                 num = 1;
-            } else if (str.startsWith("V")) {
+            } else if (str.startsWith("I+", 0)) {
+                num = 1;
+            } else if (str.startsWith("I*", 0)) {
+                num = 1;
+            } else if (str.startsWith("I/", 0)) {
+                num = 1;
+            } else if (str.startsWith("V-", 0)) {
                 num = 5;
-            } else if (str.startsWith("X")) {
+            } else if (str.startsWith("V+", 0)) {
+                num = 5;
+            } else if (str.startsWith("V*", 0)) {
+                num = 5;
+            } else if (str.startsWith("V/", 0)) {
+                num = 5;
+            } else if (str.startsWith("X-", 0)) {
                 num = 10;
+            } else if (str.startsWith("X+", 0)) {
+                num = 10;
+            } else if (str.startsWith("X*", 0)) {
+                num = 10;
+            } else if (str.startsWith("X/", 0)) {
+                num = 10;
+            }
+            else {
+                throw new SomethingWentWrong("Введите выражение, состоящее только из двух арабских или римских цифр от 1 до 10 включительно");
             }
             return num;
         }
-    public static int ending(String str, int num){  //метод для работы с римскими цифрами, который обрабатывает
-        if (str.endsWith("VIII")) {                 //конец строки и присваивает значение второй переменной
+    public static int ending(String str, int num) throws SomethingWentWrong {  //метод для работы с римскими цифрами, который обрабатывает
+        if (str.endsWith("VIII")) {                                           //конец строки и присваивает значение второй переменной
             num = 8;
         } else if (str.endsWith("III")) {
             num = 3;
@@ -115,14 +180,75 @@ public static class RomanianNums {
             num = 9;
         } else if (str.endsWith("II")) {
             num = 2;
+        }else if (str.endsWith("X")) {
+                num = 10;
         } else if (str.endsWith("I")) {
             num = 1;
         } else if (str.endsWith("V")) {
             num = 5;
-        } else if (str.endsWith("X")) {
-            num = 10;
+        }
+        else{
+            throw new SomethingWentWrong("Введите выражение, состоящее только из двух арабских или римских цифр от 1 до 10 включительно");
         }
         return num;
+    }
+    public static String romanianDigit(int n, String one, String five, String ten){
+
+        if(n >= 1)
+        {
+            if(n == 1)
+            {
+                return one;
+            }
+            else if (n == 2)
+            {
+                return one + one;
+            }
+            else if (n == 3)
+            {
+                return one + one + one;
+            }
+            else if (n==4)
+            {
+                return one + five;
+            }
+            else if (n == 5)
+            {
+                return five;
+            }
+            else if (n == 6)
+            {
+                return five + one;
+            }
+            else if (n == 7)
+            {
+                return five + one + one;
+            }
+            else if (n == 8)
+            {
+                return five + one + one + one;
+            }
+            else if (n == 9)
+            {
+                return one + ten;
+            }
+
+        }
+        return "";
+    }
+
+    public static String transl(int number){
+
+        String romanOnes = romanianDigit( number%10, "I", "V", "X");
+        number /=10;
+        String romanTens = romanianDigit( number%10, "X", "L", "C");
+        number /=10;
+        String romanHunds = romanianDigit(number%10, "C", "D", "M");
+        number /=10;
+
+        String result = romanHunds + romanTens + romanOnes;
+        return result;
+
     }
 }
 }
